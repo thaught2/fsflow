2598e9001d672f1cb2c88d965cffdc3b7690db7f
thaught2 <thaught2@gmail.com>
2025-08-07 17:56:06 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
 import Login from './pages/login';
 
+
 const App = () => (
   <BrowserRouter>
     <Routes>
