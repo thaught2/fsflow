889df9b3eb3d735ad0736c4a3bf5112149bb6382
thaught2 <thaught2@gmail.com>
2025-08-07 16:36:17 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
