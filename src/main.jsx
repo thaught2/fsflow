646aae5e75b918e9e204f3da9bb78c6104687cdc
thaught2 <thaught2@gmail.com>
2025-08-27 15:17:23 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Login from './pages/login';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/" element={<div>Home Page</div>} />
     </Routes>
   </BrowserRouter>
 );
