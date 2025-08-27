946c2e882f1db5fd58927e82b62a6f6d74489aaf
thaught2 <thaught2@gmail.com>
2025-08-27 16:39:37 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,16 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Login from './pages/login';
-
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<div>Home Page</div>} />
-    </Routes>
-  </BrowserRouter>
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import App from './App'; // optional if using <App />
 import AdminRequestForm from './pages/form';
 import Login from './pages/login';
 
-<Router>
-  <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/form" element={<AdminRequestForm />} />
-  </Routes>
-</Router>
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<AdminRequestForm />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+);
