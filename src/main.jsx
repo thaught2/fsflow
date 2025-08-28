df5fca22126ae1f6ce9232ee34f4c141cdebf167
thaught2 <thaught2@gmail.com>
2025-08-28 10:11:25 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -9,8 +10,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<AdminRequestForm />} />
+        {/* Default page = Login */}
+        <Route path="/" element={<Login />} />
+
+        {/* Explicit login route */}
         <Route path="/login" element={<Login />} />
+
+        {/* Admin request form route */}
+        <Route path="/form" element={<AdminRequestForm />} />
       </Routes>
     </Router>
   </React.StrictMode>
